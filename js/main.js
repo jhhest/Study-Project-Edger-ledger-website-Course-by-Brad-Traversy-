e4b7dc20cb6c74ec4732dfe3fc8924b80517df0b
Jan van Hest <janvanhest@outlook.com>
2019-09-19 21:00:48 +0200

    Added JS: Smootscrolling and a Googlemap
    * Used Jquery for Smooth Scrolling. 
    * Used googlemaps api to place a map.

--- js/main.js
+++ js/main.js
@@ -8,3 +8,28 @@ function initMap() {
   // The marker, positioned at Uluru
   var marker = new google.maps.Marker({ position: loc, map: map });
 }
+
+// sticky menu background: 
+window.addEventListener('scroll', function() {
+    if (window.scrollY > 150) {
+        document.querySelector('#navbar').style.opacity = 0.9;
+    } else {
+        document.querySelector('#navbar').style.opacity = 1;
+    }
+});
+
+// smooth scrolling
+$('#navbar a, .btn').on('click', function(event) {
+    if (this.hash !== '') {
+        event.preventDefault();
+
+        const hash = this.hash;
+
+        $('html, body').animate(
+            {
+                scrollTop: $(hash).offset().top -100
+            },
+            850
+        );
+    }
+});
